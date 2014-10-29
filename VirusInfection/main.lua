edc69c1b30549f32e7fccac035e2f4772bac9da4
Guillermo <guillopuyol@gmail.com>
2014-10-29 14:40:27 -0500

    Section 2 - Lecture 4: Introduction to Variables

--- VirusInfection/main.lua
+++ VirusInfection/main.lua
@@ -6,4 +6,20 @@
 
 -- Your code here
 
-print("Hello World!")
+
+
+--message = "Hello World"
+--
+--print(message)
+--
+--message2 = ", Good Day!"
+--
+--print(message..message2)
+
+virusLife = 0
+
+print("Virus Life: "..virusLife)
+
+virusLife = virusLife + 10
+
+print("Virus Life: "..virusLife)
