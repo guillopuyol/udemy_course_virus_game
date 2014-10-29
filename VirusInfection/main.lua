cce1d912501a42d79643102b6e4d404b1752b697
Guillermo <guillopuyol@gmail.com>
2014-10-29 15:27:29 -0500

    Section 2 - Lecture 6: Storing Tables Within Tables

--- VirusInfection/main.lua
+++ VirusInfection/main.lua
@@ -7,15 +7,26 @@
 -- Your code here
 
 
-virus = {["Life"]=10, ["Speed"]=30}
-
-print(virus["Life"])
-
-virus.Speed = virus.Speed + 10
-
-print(virus.Speed)
-
-virus.Name = "Cool Virus"
 
 
-print("My name is: "..virus.Name)
+
+virusColony = {}
+
+
+
+virus1 = {["Life"]=10, ["Speed"]=30, ["Color"]="Blue", ["InfectFactor"]=3}
+
+virus2 = {["Life"]=23, ["Speed"]=20, ["Color"]="Yellow", ["InfectFactor"]=1}
+
+virus3 = {["Life"]=13, ["Speed"]=25, ["Color"]="Red", ["InfectFactor"]=4}
+
+virus4 = {["Life"]=15, ["Speed"]=32, ["Color"]="Black", ["InfectFactor"]=5}
+
+
+virusColony[1]=virus1
+virusColony[2]=virus3
+virusColony[3]=virus2
+virusColony[4]=virus4
+
+
+print(virusColony[3].Color)
