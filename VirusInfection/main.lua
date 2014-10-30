11c917be6c539948eb7d825cc481855bcfc3b10f
Guillermo <guillopuyol@gmail.com>
2014-10-29 20:48:59 -0500

    Section 4 - Lecture 17: Displaying Our First Virus on the Screen
    Note: In this commit, I've added an Images folder for you to use in the game.

--- VirusInfection/main.lua
+++ VirusInfection/main.lua
@@ -1,84 +1,109 @@
------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
+----
+---- main.lua
+----
+-------------------------------------------------------------------------------------------
 --
--- main.lua
+---- Your code here
 --
------------------------------------------------------------------------------------------
+--
+--
+--
+--virusColony = {}
+--
+--local maxSpeed = 40
+--
+--virus1 = {["Life"]=1, ["Speed"]=maxSpeed, ["Color"]="Blue", ["InfectFactor"]=3}
+--
+--virus2 = {["Life"]=23, ["Speed"]=maxSpeed, ["Color"]="Yellow", ["InfectFactor"]=1}
+--
+--virus3 = {["Life"]=13, ["Speed"]=maxSpeed, ["Color"]="Red", ["InfectFactor"]=4}
+--
+--virus4 = {["Life"]=15, ["Speed"]=maxSpeed, ["Color"]="Black", ["InfectFactor"]=5}
+--
+--
+--virusColony[1]=virus1
+--virusColony[2]=virus3
+--virusColony[3]=virus2
+--virusColony[4]=virus4
+--
+--
+--for i=1,#virusColony do
+--    
+--    if type(virusColony[i])=="table" then
+--    
+--        print("We're now checking the "..virusColony[i].Color.." virus")
+--
+--        repeat
+--             if virusColony[i].Life > 30 then
+--                 print("Virus Died!")
+--             elseif virusColony[i].Life == 30 then
+--                 print("Virus is about to die!")
+--             elseif virusColony[i].Life >= 25 and virusColony[i].Life <= 30 then
+--                 print("Virus is getting old!")
+--             else
+--                 print("Virus is in good health.")
+--             end
+--
+--             virusColony[i].Life = virusColony[i].Life + 1
+----             print("Virus Age: "..virusColony[i].Life)
+--
+--         until virusColony[i].Life>=32 
+--         
+--    end
+--end
+--
+--
+--function changeVirusSpeed(virus, deltaSpeed)
+--    
+--    local maxSpeed = 70
+--    
+--    virus.Speed = virus.Speed + deltaSpeed
+--    
+--    print("MaxSpeed inside the function: "..maxSpeed)
+--
+--end
+--
+--
+--function getVirusSpeed(virus)
+--    return virus.Speed
+--end
+--
+--
+--print("Original MaxSpeed: "..maxSpeed)
+--
+--print("Original Speed: "..virusColony[2].Speed)
+--
+--changeVirusSpeed(virusColony[2], 40)
+--
+--print("MaxSpeed after the function: "..maxSpeed)
+--
+--print("Original Speed: "..virusColony[2].Speed)
+--
+--
+--print("Virus 3 has a speed of: "..getVirusSpeed(virusColony[3]))
 
--- Your code here
 
 
 
 
-virusColony = {}
-
-local maxSpeed = 40
-
-virus1 = {["Life"]=1, ["Speed"]=maxSpeed, ["Color"]="Blue", ["InfectFactor"]=3}
-
-virus2 = {["Life"]=23, ["Speed"]=maxSpeed, ["Color"]="Yellow", ["InfectFactor"]=1}
-
-virus3 = {["Life"]=13, ["Speed"]=maxSpeed, ["Color"]="Red", ["InfectFactor"]=4}
-
-virus4 = {["Life"]=15, ["Speed"]=maxSpeed, ["Color"]="Black", ["InfectFactor"]=5}
 
 
-virusColony[1]=virus1
-virusColony[2]=virus3
-virusColony[3]=virus2
-virusColony[4]=virus4
 
 
-for i=1,#virusColony do
-    
-    if type(virusColony[i])=="table" then
-    
-        print("We're now checking the "..virusColony[i].Color.." virus")
-
-        repeat
-             if virusColony[i].Life > 30 then
-                 print("Virus Died!")
-             elseif virusColony[i].Life == 30 then
-                 print("Virus is about to die!")
-             elseif virusColony[i].Life >= 25 and virusColony[i].Life <= 30 then
-                 print("Virus is getting old!")
-             else
-                 print("Virus is in good health.")
-             end
-
-             virusColony[i].Life = virusColony[i].Life + 1
---             print("Virus Age: "..virusColony[i].Life)
-
-         until virusColony[i].Life>=32 
-         
-    end
-end
+local screen = display.newGroup()
 
 
-function changeVirusSpeed(virus, deltaSpeed)
-    
-    local maxSpeed = 70
-    
-    virus.Speed = virus.Speed + deltaSpeed
-    
-    print("MaxSpeed inside the function: "..maxSpeed)
+local virus = display.newImageRect( screen, "/Images/RedVirus@2x.png", 64, 64 )
+ 
 
-end
+virus.x = 160
+virus.y = 240
 
 
-function getVirusSpeed(virus)
-    return virus.Speed
-end
 
+local virus2 = display.newImageRect( screen, "/Images/BlueVirus@2x.png", 64, 64 )
+ 
 
-print("Original MaxSpeed: "..maxSpeed)
-
-print("Original Speed: "..virusColony[2].Speed)
-
-changeVirusSpeed(virusColony[2], 40)
-
-print("MaxSpeed after the function: "..maxSpeed)
-
-print("Original Speed: "..virusColony[2].Speed)
-
-
-print("Virus 3 has a speed of: "..getVirusSpeed(virusColony[3]))
+virus.x = 160
+virus.y = 140
