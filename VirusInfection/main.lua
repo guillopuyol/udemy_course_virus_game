c9d7295e3040f6d8850b71b641099f34c758b88d
Guillermo <guillopuyol@gmail.com>
2014-10-31 10:53:58 -0500

    Section 4 - Lecture 34: Background Music and Sound Effects

--- VirusInfection/main.lua
+++ VirusInfection/main.lua
@@ -1,95 +1,4 @@
 require "CiderDebugger";-------------------------------------------------------------------------------------------
-----
----- main.lua
-----
--------------------------------------------------------------------------------------------
---
----- Your code here
---
---
---
---
---virusColony = {}
---
---local maxSpeed = 40
---
---virus1 = {["Life"]=1, ["Speed"]=maxSpeed, ["Color"]="Blue", ["InfectFactor"]=3}
---
---virus2 = {["Life"]=23, ["Speed"]=maxSpeed, ["Color"]="Yellow", ["InfectFactor"]=1}
---
---virus3 = {["Life"]=13, ["Speed"]=maxSpeed, ["Color"]="Red", ["InfectFactor"]=4}
---
---virus4 = {["Life"]=15, ["Speed"]=maxSpeed, ["Color"]="Black", ["InfectFactor"]=5}
---
---
---virusColony[1]=virus1
---virusColony[2]=virus3
---virusColony[3]=virus2
---virusColony[4]=virus4
---
---
---for i=1,#virusColony do
---    
---    if type(virusColony[i])=="table" then
---    
---        print("We're now checking the "..virusColony[i].Color.." virus")
---
---        repeat
---             if virusColony[i].Life > 30 then
---                 print("Virus Died!")
---             elseif virusColony[i].Life == 30 then
---                 print("Virus is about to die!")
---             elseif virusColony[i].Life >= 25 and virusColony[i].Life <= 30 then
---                 print("Virus is getting old!")
---             else
---                 print("Virus is in good health.")
---             end
---
---             virusColony[i].Life = virusColony[i].Life + 1
-----             print("Virus Age: "..virusColony[i].Life)
---
---         until virusColony[i].Life>=32 
---         
---    end
---end
---
---
---function changeVirusSpeed(virus, deltaSpeed)
---    
---    local maxSpeed = 70
---    
---    virus.Speed = virus.Speed + deltaSpeed
---    
---    print("MaxSpeed inside the function: "..maxSpeed)
---
---end
---
---
---function getVirusSpeed(virus)
---    return virus.Speed
---end
---
---
---print("Original MaxSpeed: "..maxSpeed)
---
---print("Original Speed: "..virusColony[2].Speed)
---
---changeVirusSpeed(virusColony[2], 40)
---
---print("MaxSpeed after the function: "..maxSpeed)
---
---print("Original Speed: "..virusColony[2].Speed)
---
---
---print("Virus 3 has a speed of: "..getVirusSpeed(virusColony[3]))
-
-
-
-
-
-
-
-
 
 local screen
 
@@ -102,6 +11,11 @@ gameVars.maxVirusOnScreen = 20
 gameVars.currentLevel = 1
 gameVars.currentLevelBeat = false
 gameVars.levels = {}
+
+local bgMusic
+local bgMusicChannel
+
+local virusPopSound, virusPopSoundChannel
 
 local drawLevel
 
@@ -261,6 +175,8 @@ local function createVirus(params)
                 local event = {name="virusDied", target=virus}
                 virus:dispatchEvent(event)
                 
+                audio.play(virusPopSound)
+                
         end
         
         return true
@@ -395,6 +311,18 @@ local function initScreen()
     local background = display.newImageRect(screen, "/Images/background@2x.png", contentWidth, contentHeight )
     background.x = contentWidth/2
     background.y = contentHeight/2
+    
+    
+    --Load Music
+    bgMusic = audio.loadStream("Sounds/music.wav")
+    bgMusicChannel = audio.findFreeChannel()
+    
+    audio.play(bgMusic, {channel=bgMusicChannel, loops=-1, fadeIn=3000})
+    
+    
+    --Load Virus Sound
+    virusPopSound = audio.loadSound("Sounds/pop.wav")
+    virusPopSoundChannel = audio.findFreeChannel()
     
     drawLevel()
     
